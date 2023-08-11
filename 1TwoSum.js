d1e58b1932bd49d4736179275f444732d6790621
ladotorchinava <ladotorchinava17@gmail.com>
2023-08-11 19:42:38 +0400

    Create 1TwoSum.js
    1TwoSum

---
+++ 1TwoSum.js
@@ -0,0 +1,16 @@
+var twoSum = function(nums, target) {
+    const numToIndex = {};  
+  
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;  
+        if (complement in numToIndex) {
+            return [numToIndex[complement], i];
+        }
+        
+        numToIndex[num] = i;
+    }
+
+
+};
